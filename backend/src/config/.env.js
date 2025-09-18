8c2cb37f8b802e32e612355d1f3a2929e803d66a
aniravla <aniravla@yahoo.com>
2025-09-18 17:10:01 +0300

    Setup ENV

---
+++ backend/src/config/.env.js
@@ -0,0 +1,16 @@
+import dotenv from dotenv';
+
+dotenv.config();
+
+export const ENV = {
+  PORT: process.env.PORT || 3000,
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase',
+  CLERK_API_KEY: process.env.CLERK_API_KEY || '',
+  CLERK_API_URL: process.env.CLERK_API_URL || '',
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || '',
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || '',
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || '',
+  ARCJET_API_KEY: process.env.ARCJET_API_KEY || '',
+};
+
+export default ENV;
