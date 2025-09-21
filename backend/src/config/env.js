87e2b3bcf5a54e30080775b6f4852ca1e607f377
aniravla <aniravla@yahoo.com>
2025-09-21 12:08:12 +0300

    configure db connection

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,10 +1,10 @@
-import dotenv from dotenv';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const ENV = {
   PORT: process.env.PORT || 3000,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase',
   CLERK_API_KEY: process.env.CLERK_API_KEY || '',
   CLERK_API_URL: process.env.CLERK_API_URL || '',
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || '',
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import { ENV } from "config/env.js";
+import { ENV } from "./config/env.js";
+import { connectDB } from './config/db.js';
 
 const app = express();
+
+connectDB();
+
+app.get('/', (req, res) =>  res.send('Hello From Server!'));
+
 
 app.listen(ENV.PORT, () => 
   console.log('Server is running on PORT: ', ENV.PORT)
