0715b80f9bec953fe66c1646f3a599d57f0ebfd7
aniravla <aniravla@yahoo.com>
2025-09-22 15:44:58 +0300

    Search page created

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" />
       </Stack>
     </ClerkProvider>
   );
